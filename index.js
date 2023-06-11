547cf58ae4789b90e84b8529f5561757f54cba88
최경보 <chlrudqh97@naver.com>
2023-06-11 23:42:53 +0900

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello world!")
+
+var figlet = require("figlet");
+
+figlet("Jo Yeo-Jeong ", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
