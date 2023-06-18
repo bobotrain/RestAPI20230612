95a0e90f44309e92859d08c10f0be1b8a0db92ef
K.B CHOI <chlrudqh97@naver.com>
2023-06-18 23:54:04 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-//console.log("Hello world!")
+const express = require('express')
+const app = express()
 
-var figlet = require("figlet");
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-figlet("Jo Yeo-Jeong ", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
+//:는 이 값을 파라미터 변수로 받는다는 뜻
+app.get('/user/:id', function (req, res) {
+
+  //내가 입력한 params값
+  const q = req.params
+  console.log(q.id)
+
+  res.json({'userid': q.id })
+})
+
+
+app.get('/dog', function (req, res) {
+  res.json({'sound': '멍멍'})
+})
+
+
+//사운드 구별
+app.get('/sound/:name', function (req, res) {
+  const {name} = req.params
+
+  //js문법 사용가능
+  if(name =="dog") {
+    res.json({'sound' : '멍멍'})
+  }else if(name =="cat") {
+    res.json({'sound' : '야옹'})
+  }else if(name =="pig") {
+    res.json({'sound' : '꿀꿀'})
+  }else {
+    res.json({'sound' : '알수없음'})
   }
-  console.log(data);
-});
+})
+
+app.listen(3000) 
